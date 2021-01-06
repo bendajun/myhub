4221b2e0d5b86a2924556d33052c2f0c14700fb8
dudapeng <1442766925@qq.com>
2021-01-06 17:54:09 +0800

    koa的ctx类型判断

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
-const Koa = require('koa')
-
+import Koa from 'koa'
+import { BaseContext } from 'koa'
 const app = new Koa()
+
+app.use((ctx: BaseContext) => {
+  ctx.body = 'hello world'
+})
 
 app.listen(8989, () => {
   console.log('app is running port 8989 ~')
